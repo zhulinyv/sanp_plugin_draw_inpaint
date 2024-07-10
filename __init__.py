55ac324079bf8744a33ea4249e8fa7d026d8ad38
zhulinyv <zhulinyv2005@outlook.com>
2024-07-10 20:51:41 +0800

    :tada: 首次提交

--- __init__.py
+++ __init__.py
@@ -2,35 +2,164 @@ from pathlib import Path
 
 import gradio as gr
 
-from plugins.t2i.sanp_plugin_template.utils import t2i
-from utils.utils import open_folder
+from plugins.inpaint.sanp_plugin_draw_inpaint.utils import for_webui as draw_inpaint
+from utils.env import env
+from utils.utils import (
+    NOISE_SCHEDULE,
+    RESOLUTION,
+    SAMPLER,
+    open_folder,
+    read_json,
+    return_random,
+)
 
-
-def plugin_template(name="模板插件", description="描述", func=None):
-    with gr.Tab(name):
-        with gr.Row():
-            with gr.Column(scale=8):
-                gr.Markdown(f"> {description}")
-            folder = gr.Textbox(Path("./output/t2i"), visible=False)
-            open_folder_ = gr.Button("打开保存目录", scale=1)
-            open_folder_.click(open_folder, inputs=folder)
-        with gr.Row():
-            generate_button = gr.Button("无限生成")
-            stop_button = gr.Button("停止生成")
-        otp_img = gr.Image()
-        cancel_event = otp_img.change(
-            fn=func,
-            inputs=None,
-            outputs=otp_img,
-            show_progress="hidden",
-        )
-        generate_button.click(
-            fn=func,
-            inputs=None,
-            outputs=otp_img,
-        )
-        stop_button.click(None, None, None, cancels=[cancel_event])
+webui_language = read_json(f"./files/languages/{env.webui_lang}/webui.json")
 
 
 def plugin():
-    plugin_template("模板插件", "这是一段描述说明或教程", t2i)
+    with gr.Tab("涂鸦重绘"):
+
+        def open_output_folder_block(output_folder):
+            open_output_folder_folder = gr.Button(
+                webui_language["t2i"]["open_folder"], scale=1
+            )
+            open_output_folder_folder.click(
+                open_folder,
+                inputs=gr.Textbox(Path(f"./output/{output_folder}"), visible=False),
+            )
+
+        with gr.Row():
+            with gr.Column(scale=8):
+                gr.Markdown(
+                    "> 对涂鸦的部分进行局部重绘(先点击重绘图片组件下方的图片按钮上传图片, 在点击绘画按钮进行局部涂抹)"
+                )
+            open_output_folder_block("inpaint")
+        with gr.Column():
+            with gr.Column():
+                draw_inpaint_positive_input = gr.Textbox(
+                    value=webui_language["example"]["positive"],
+                    lines=2,
+                    label=webui_language["t2i"]["positive"],
+                )
+                with gr.Row():
+                    draw_inpaint_negative_input = gr.Textbox(
+                        value=webui_language["example"]["negative"],
+                        lines=3,
+                        label=webui_language["t2i"]["negative"],
+                        scale=3,
+                    )
+                    draw_inpaint_generate_button = gr.Button(
+                        value=webui_language["t2i"]["generate_button"], scale=1
+                    )
+            with gr.Row():
+                draw_inpaint_input_path = gr.Textbox(
+                    value="", label=webui_language["inpaint"]["input_path"], scale=5
+                )
+                draw_inpaint_mask_path = gr.Textbox(
+                    value="", label=webui_language["inpaint"]["mask_path"], scale=5
+                )
+                draw_inpaint_batch_switch = gr.Radio(
+                    [True, False],
+                    value=False,
+                    label=webui_language["i2i"]["open_button"],
+                    scale=1,
+                )
+            with gr.Row():
+                draw_inpaint_input_image = gr.ImageEditor(
+                    sources=["upload", "clipboard", "webcam"],
+                    type="pil",
+                    label=webui_language["inpaint"]["inpaint_img"],
+                )
+                with gr.Column():
+                    draw_inpaint_output_information = gr.Textbox(
+                        label=webui_language["i2i"]["output_info"]
+                    )
+                    draw_inpaint_output_image = gr.Image()
+            with gr.Column():
+                with gr.Row():
+                    draw_inpaint_resolution = gr.Dropdown(
+                        RESOLUTION,
+                        value="832x1216",
+                        label=webui_language["t2i"]["resolution"],
+                    )
+                    draw_inpaint_sampler = gr.Dropdown(
+                        SAMPLER,
+                        value="k_euler",
+                        label=webui_language["t2i"]["sampler"],
+                    )
+                    draw_inpaint_noise_schedule = gr.Dropdown(
+                        NOISE_SCHEDULE,
+                        value="native",
+                        label=webui_language["t2i"]["noise_schedule"],
+                    )
+                with gr.Row():
+                    draw_inpaint_strength = gr.Slider(
+                        minimum=0,
+                        maximum=1,
+                        value=0.5,
+                        step=0.1,
+                        label=webui_language["i2i"]["strength"],
+                    )
+                    draw_inpaint_noise = gr.Slider(
+                        minimum=0,
+                        maximum=1,
+                        value=0,
+                        step=0.1,
+                        label=webui_language["i2i"]["noise"],
+                    )
+                    draw_inpaint_scale = gr.Slider(
+                        minimum=0,
+                        maximum=10,
+                        value=5,
+                        step=0.1,
+                        label=webui_language["t2i"]["scale"],
+                    )
+                    draw_inpaint_steps = gr.Slider(
+                        minimum=0,
+                        maximum=50,
+                        value=28,
+                        step=1,
+                        label=webui_language["t2i"]["steps"],
+                    )
+                with gr.Row():
+                    draw_inpaint_sm = gr.Radio(
+                        [True, False], value=False, label="sm", scale=2
+                    )
+                    draw_inpaint_sm_dyn = gr.Radio(
+                        [True, False],
+                        value=False,
+                        label=webui_language["t2i"]["smdyn"],
+                        scale=2,
+                    )
+                    with gr.Column(scale=1):
+                        draw_inpaint_seed = gr.Textbox(
+                            value="-1", label=webui_language["t2i"]["seed"], scale=7
+                        )
+                        draw_inpaint_random_button = gr.Button(
+                            value="♻️", size="sm", scale=1
+                        )
+                        draw_inpaint_random_button.click(
+                            return_random, inputs=None, outputs=draw_inpaint_seed
+                        )
+        draw_inpaint_generate_button.click(
+            fn=draw_inpaint,
+            inputs=[
+                draw_inpaint_input_path,
+                draw_inpaint_mask_path,
+                draw_inpaint_input_image,
+                draw_inpaint_batch_switch,
+                draw_inpaint_positive_input,
+                draw_inpaint_negative_input,
+                draw_inpaint_resolution,
+                draw_inpaint_sampler,
+                draw_inpaint_noise_schedule,
+                draw_inpaint_strength,
+                draw_inpaint_noise,
+                draw_inpaint_scale,
+                draw_inpaint_steps,
+                draw_inpaint_sm,
+                draw_inpaint_sm_dyn,
+                draw_inpaint_seed,
+            ],
+            outputs=[draw_inpaint_output_image, draw_inpaint_output_information],
+        )
--- utils.py
+++ utils.py
@@ -1,42 +1,111 @@
 import random
 
-from src.t2i import t2i_by_hand
-from utils.env import env
-from utils.utils import format_str, sleep_for_cool
+import ujson as json
+from loguru import logger
+
+from src.image2image import prepare_json
+from utils.imgtools import (
+    change_the_mask_color_to_white,
+    get_img_info,
+    img_to_base64,
+    revert_img_info,
+)
+from utils.utils import (
+    file_namel2pathl,
+    file_path2list,
+    file_path2name,
+    generate_image,
+    inquire_anlas,
+    save_image,
+)
 
 
-def prompt():
-    positive = ...
-    negative = ...
-    return format_str(positive), format_str(negative)
+def for_webui(
+    input_path,
+    mask_path,
+    inpaint_input_image,
+    open_button,
+    inpaint_positive_input,
+    inpaint_negative_input,
+    inpaint_resolution,
+    inpaint_sampler,
+    inpaint_noise_schedule,
+    inpaint_strength,
+    inpaint_noise,
+    inpaint_scale,
+    inpaint_steps,
+    inpaint_sm,
+    inpaint_sm_dyn,
+    inpaint_seed,
+):
+    logger.warning(f"剩余水晶: {inquire_anlas()}")
+    if open_button:
+        main(input_path, mask_path)
+        return None, "处理完成, 图片已保存到 ./output/inpaint..."
+    else:
+        (inpaint_input_image["composite"]).save("./output/temp_inpaint_img.png")
+        (inpaint_input_image["layers"][0]).save("./output/temp_inpaint_mask.png")
+        change_the_mask_color_to_white("./output/temp_inpaint_mask.png")
+
+        info = {
+            "Software": "NovelAI",
+            "Comment": json.dumps(
+                {
+                    "prompt": inpaint_positive_input,
+                    "steps": inpaint_steps,
+                    "height": int(inpaint_resolution.split("x")[1]),
+                    "width": int(inpaint_resolution.split("x")[0]),
+                    "scale": inpaint_scale,
+                    "seed": (
+                        random.randint(1000000000, 9999999999)
+                        if inpaint_seed == "-1"
+                        else int(inpaint_seed)
+                    ),
+                    "noise_schedule": inpaint_noise_schedule,
+                    "sampler": inpaint_sampler,
+                    "sm": inpaint_sm,
+                    "sm_dyn": inpaint_sm_dyn,
+                    "uc": inpaint_negative_input,
+                }
+            ),
+        }
+
+        revert_img_info(None, "./output/temp_inpaint_img.png", info)
+
+        logger.info("开始重绘...")
+        path = inpaint(
+            "./output/temp_inpaint_img.png",
+            "./output/temp_inpaint_mask.png",
+            inpaint_strength,
+            inpaint_noise,
+        )
+    return path, None
 
 
-def t2i():
-    positive, negative = prompt()
-    resolution = (
-        random.choice(["832x1216", "1024x1024", "1216x832"])
-        if env.img_size == -1
-        else "{}x{}".format(env.img_size[0], env.img_size[1])
+def inpaint(img_path, mask_path, *args):
+    imginfo = get_img_info(img_path)
+    json_for_inpaint = prepare_json(imginfo, img_path)
+    json_for_inpaint["parameters"]["mask"] = img_to_base64(mask_path)
+    json_for_inpaint["model"] = "nai-diffusion-3-inpainting"
+    json_for_inpaint["action"] = "infill"
+    if args:
+        json_for_inpaint["strength"] = args[0]
+        json_for_inpaint["noise"] = args[1]
+
+    saved_path = save_image(
+        generate_image(json_for_inpaint),
+        "inpaint",
+        (json.loads(imginfo["Comment"]))["seed"],
+        "None",
+        "None",
     )
-    scale = env.scale
-    sampler = env.sampler
-    noise_schedule = env.noise_schedule
-    steps = env.steps
-    sm = env.sm
-    sm_dyn = env.sm_dyn
-    seed = random.randint(1000000000, 9999999999) if env.seed == -1 else env.seed
-    img = t2i_by_hand(
-        positive,
-        negative,
-        resolution,
-        scale,
-        sampler,
-        noise_schedule,
-        steps,
-        sm,
-        sm_dyn,
-        seed,
-        times=1,
-    )
-    sleep_for_cool(env.t2i_cool_time - 6, env.t2i_cool_time + 6)
-    return img
+
+    return saved_path
+
+
+def main(img_folder, mask_folder):
+    file_list = file_namel2pathl(file_path2list(img_folder), img_folder)
+
+    for file in file_list:
+        logger.info(f"正在处理: {file}")
+        inpaint(file, f"{mask_folder}/{file_path2name(file)}")
